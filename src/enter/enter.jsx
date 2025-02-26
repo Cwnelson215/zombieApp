f30b390264d915ba44b0b91582dd211302e6a77e
Carter Nelson <cartern215@gmail.com>
2025-02-25 23:42:34 -0700

    finished enter_joincode.jsx for reactp2

--- src/enter/enter.jsx
+++ src/enter/enter.jsx
@@ -2,21 +2,43 @@ import React from 'react';
 import './enter_joincode.css';
 
 export function Enter() {
+    const [joinCode, setJoinCode] = React.useState("")
+    const [nickname, setNickname] = React.useState("")
+    const [message, setMessage] = React.useState("")
+
+    const handleCode = (event) => {
+        setJoinCode(event.target.value);
+    };
+    const submitJoinCode = () => {
+        localStorage.setItem('join code', joinCode);
+    };
+    const handleNickname = (event) => {
+        setNickname(event.target.value);
+    };
+    const submitNickname = () => {
+        localStorage.setItem('player nickname', nickname);
+    };
+    const handleMessage = () => {
+        setMessage("Gravitar Engaged");
+    };
+
     return (
         <main>
             <div className="code">
                 <label name="code">Enter Join Code:</label>
-                <input type="text" id="code" name="code"/>
+                <input type="text" id="code" name="code" onChange={handleCode}/>
+                <button className="enter" onClick={submitJoinCode}>Enter</button>
+                <p className="nickname">Choose Your Nickname</p>
             </div>
-            
-            <p className="nickname">Choose Your Nickname</p>
 
             <div className="nickname_input">
                 <label forhtml="Nickname">Enter Nickname:</label>
-                <input type="text" id="Nickname" name="Nickname"/>
+                <input type="text" id="Nickname" name="Nickname" onChange={handleNickname}/>
+                <input className="n_button" type='button' value="Submit Nickname" onClick={submitNickname}/>
             </div>
             <div className="button">
-                <input type="button" value="Use Gravatar"/>
+                <input type="button" value="Use Gravatar" onClick={handleMessage} />
+                {message && <p>{message}</p>}
             </div>
         </main>
     );
